96628ee6dd6332d696233971d627699fbba4d489
Jared Reando and Phil Mass <jared@>
2018-10-15 14:54:58 -0700

    add reverse initials to sentence

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   var bookends = function(str) {
     return (str.charAt(0) + str.charAt(str.length - 1)).toUpperCase();
   }
+
   var reverse = function(str) {
     return str.split("").reverse().join("");
   }
 
-  var sentence = prompt("Enter a sentence:");
-  var initials = bookends(sentence);
-  console.log(sentence + " -> " + initials);
-  console.log(reverse(initials))
+  var initials = function(str) {
+    return str + reverse(bookends(str));
+  }
 
+  var sentence = prompt("Enter a sentence:");
+  var reversedInitials = initials(sentence)
+  console.log(sentence + " -> " + reversedInitials);
 });
